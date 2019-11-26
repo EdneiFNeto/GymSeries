7c1ef8698d58739da3ea623c72f395c0ddafd533
EdneiFNeto <edneifneto@gmail.com>
2019-11-26 13:01:35 -0300

    pai chart adicionada

--- app/src/main/java/com/gymseries/fragments/ChartFragment.kt
+++ app/src/main/java/com/gymseries/fragments/ChartFragment.kt
@@ -1,13 +1,26 @@
 package com.gymseries.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.gymseries.R
+import com.gymseries.utils.ActionBarUtils
+import com.gymseries.utils.ResourcesUtils
 
 class ChartFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var supportActionBar = (activity as AppCompatActivity).supportActionBar
+        ActionBarUtils.show(supportActionBar)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +28,33 @@ class ChartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return LayoutInflater.from(context).inflate(R.layout.layout_settings, container, false)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+        val item = menu.add(ResourcesUtils.getString(context, R.string.title_dialog_chart))
+        item.setIcon(R.drawable.ic_calculator)
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        item.setOnMenuItemClickListener {
+
+            if (context != null) {
+
+                var builder = AlertDialog.Builder(context!!)
+                builder.setIcon(R.drawable.ic_calculator)
+                builder.setTitle(ResourcesUtils.getString(context, R.string.title_calcular_imc))
+                val myView = LayoutInflater.from(context).inflate(R.layout.layout_dialog_medidas, null)
+                builder.setView(myView)
+
+                var peso = myView.findViewById<EditText>(R.id.edt_text_peso_imc)
+                var altura = myView.findViewById<EditText>(R.id.edt_text_altura_imc)
+                var dialog = builder.create()
+                dialog.show()
+            }
+
+            true
+        }
+
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
