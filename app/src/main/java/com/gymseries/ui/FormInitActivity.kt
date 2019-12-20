9bb2dc0bf0acb0192db48a8278d88e4a05a2771b
EdneiFNeto <edneifneto@gmail.com>
2019-12-20 07:05:24 -0300

    form init

---
+++ app/src/main/java/com/gymseries/ui/FormInitActivity.kt
@@ -0,0 +1,13 @@
+package com.gymseries.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gymseries.R
+
+class FormInitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_init)
+    }
+}
